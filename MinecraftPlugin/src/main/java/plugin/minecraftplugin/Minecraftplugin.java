3e082bd5cb9bca2c151bc0da558362ea15763e04
Mazedias <69587868+Mazedias@users.noreply.github.com>
2020-12-05 11:45:52 +0100

    Add files via upload

---
+++ MinecraftPlugin/src/main/java/plugin/minecraftplugin/Minecraftplugin.java
@@ -0,0 +1,17 @@
+package plugin.minecraftplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Minecraftplugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
