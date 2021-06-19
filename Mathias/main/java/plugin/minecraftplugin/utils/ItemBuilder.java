8f14c64fe01ae00f0f42fae46e03d6bc7f5b3cd6
Mazedias <69587868+Mazedias@users.noreply.github.com>
2021-06-19 11:07:31 +0200

    Update ItemBuilder.java

--- Mathias/main/java/plugin/minecraftplugin/utils/ItemBuilder.java
+++ Mathias/main/java/plugin/minecraftplugin/utils/ItemBuilder.java
@@ -12,6 +12,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+/*
+Diese Datei ist nicht von @Mazedias (Siehe oben), Sie dient dazu Items in einem Inventar leichter zu erstellen
+*/
+
 package plugin.minecraftplugin.utils;
 
 import com.google.gson.Gson;
